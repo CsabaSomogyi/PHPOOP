09dea58c8a01604620e1f9b36ff28218a29cf043
Csaba Somogyi <csaba.somogyi10@gmail.com>
2018-02-22 15:37:28 +0100

    1st video, std class

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<? php ?>
+<?php
+
+// 1st video
+// stdClass php generic class (C# object )
+$object = new stdClass;
+$object->names = ['Jhon', 'Billy' , 'Susan'];
+foreach ($object->names as $name ) {
+	echo $name . '<br>';
+}
+// 1st video END
+?>
